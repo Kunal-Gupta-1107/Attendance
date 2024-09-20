440d996565e67a11209439dfce5ae29118ab19ec
Kunal Gupta <sgkunal123@hotmail.com>
2024-09-20 07:05:25 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,15 +8,21 @@ const urlsToCache = [
   '/icon-large.png'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached response if found, otherwise fetch from network
+        return response || fetch(event.request).then((networkResponse) => {
+          if (event.request.mode === 'navigate') {
+            return caches.match('/index.html');
+          }
+          return networkResponse;
+        });
       })
   );
 });
+
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
