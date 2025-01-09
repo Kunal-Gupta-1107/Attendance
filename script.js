d15238c6faeea61e484fccdb1ebbd4464501a1a0
Kunal Gupta <sgkunal123@hotmail.com>
2025-01-09 15:59:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -269,6 +269,7 @@ document.addEventListener('DOMContentLoaded', () => {
                                 registration.showNotification("I Welcome you!", {
                                     body: "Thanks for enabling me.😊",
                                     icon: "https://kunal-gupta-1107.github.io/Attendance/icon.png",
+                                    badge: "/Attendance/badge.png",
                                     vibrate: [200, 100, 200] 
                                 });
                             });
