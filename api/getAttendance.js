4f56f1a2a6b7a85474fc50b7f9bbd85506294d81
Kunal Gupta <sgkunal123@hotmail.com>
2025-01-25 14:05:32 +0530

    Update getAttendance.js

--- api/getAttendance.js
+++ api/getAttendance.js
@@ -6,4 +6,5 @@ export default function handler(req, res) {
         storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
         messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
         appId: process.env.FIREBASE_APP_ID
-    });}
+    });
+}
